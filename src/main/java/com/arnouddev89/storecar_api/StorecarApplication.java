3c31cdad8a98a2c49c756d2233a57e5120f0140b
ArnoudDev89 <arnouddev89@gmail.com>
2026-01-15 23:09:01 -0300

    atualização de projeto

--- src/main/java/com/arnouddev89/storecar_api/StorecarApplication.java
+++ src/main/java/com/arnouddev89/storecar_api/StorecarApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StorecarApiApplication {
+public class StorecarApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StorecarApiApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(StorecarApplication.class, args);
 	}
-
 }
